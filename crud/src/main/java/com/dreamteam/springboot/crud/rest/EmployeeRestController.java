57506bd0a736bd561f1c930d979e346fa84ac3c6
sfanpro <st.sergeyf@gmail.com>
2019-04-30 19:41:50 +0300

    added get post rest methods

--- crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,21 @@ public class EmployeeRestController {
 		return employeeService.findAll();
 	}
 
+	// add mapping for GET /employees/{employeeId}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not fouund - " + employeeId);
+		}
+		return theEmployee;
+	}
+
+	// add mapping for POST /employees- add new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
 }
