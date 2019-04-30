9d24cd2c7a7ec16a640e056915e8ea77d36dd6df
sfanpro <st.sergeyf@gmail.com>
2019-04-30 18:36:23 +0300

    find by id, save and delete methods

--- crud/src/main/java/com/dreamteam/springboot/crud/dao/EmployeeDAO.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/dao/EmployeeDAO.java
@@ -7,4 +7,10 @@ import com.dreamteam.springboot.crud.entity.Employee;
 public interface EmployeeDAO {
 
 	public List<Employee> findAll();
+
+	public Employee findById(int theId);
+
+	public void save(Employee theEmployee);
+
+	public void deleteBeId(int theId);
 }
--- crud/src/main/java/com/dreamteam/springboot/crud/dao/EmployeeDAOHibernateImpl.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/dao/EmployeeDAOHibernateImpl.java
@@ -8,7 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.dreamteam.springboot.crud.entity.Employee;
 
@@ -25,7 +24,6 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	}
 
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 		// get the current hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
@@ -40,4 +38,35 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 		return employees;
 	}
 
+	@Override
+	public Employee findById(int theId) {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// get the employee
+		Employee theEmployee = currentSession.get(Employee.class, theId);
+
+		// return the employee
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// save employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	public void deleteBeId(int theId) {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// delete object with the peimary key
+		Query theQuery = currentSession.createQuery("delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		theQuery.executeUpdate();
+	}
 }
