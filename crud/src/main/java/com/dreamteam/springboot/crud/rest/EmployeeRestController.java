9fb7a14c78ac2ad2dec2706bb40cf7f0b843eb98
sfanpro <st.sergeyf@gmail.com>
2019-04-30 19:03:10 +0300

    created EmployeeService and Impl

--- crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
@@ -7,25 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dreamteam.springboot.crud.dao.EmployeeDAO;
 import com.dreamteam.springboot.crud.entity.Employee;
+import com.dreamteam.springboot.crud.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 
-	// inject employee dao
 	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	public EmployeeRestController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 
 	// expose "/employees" and return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
 	}
 
 }
---
+++ crud/src/main/java/com/dreamteam/springboot/crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.dreamteam.springboot.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dreamteam.springboot.crud.dao.EmployeeDAO;
+import com.dreamteam.springboot.crud.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteBeId(theId);
+	}
+
+}
