8296e6c47a9f84e9d9c9f676ae6fae8c35be2bcf
sfanpro <st.sergeyf@gmail.com>
2019-05-02 11:23:11 +0300

    add mapping for DELETE

--- crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.dreamteam.springboot.crud.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,16 @@ public class EmployeeRestController {
 	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		employeeService.save(theEmployee);
 		return theEmployee;
+	}
+
+	// add mapping for DELETE /employees/{employeeId}
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not fouund - " + employeeId);
+		}
+		employeeService.deleteById(employeeId);
+		return "Deleted employee  id - " + employeeId;
 	}
 }
