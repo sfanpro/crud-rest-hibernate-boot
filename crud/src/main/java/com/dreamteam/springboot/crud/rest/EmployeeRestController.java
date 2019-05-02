64f12fc58304267b0f742542c5a419b0a478f751
sfanpro <st.sergeyf@gmail.com>
2019-05-02 11:17:01 +0300

    // add mapping for PUT

--- crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
+++ crud/src/main/java/com/dreamteam/springboot/crud/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,13 @@ public class EmployeeRestController {
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee theEmployee) {
 		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+
+	// add mapping for PUT /employees = update existing employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		employeeService.save(theEmployee);
 		return theEmployee;
 	}
